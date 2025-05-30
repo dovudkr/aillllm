df09aac1dbd04e6784a4076071e17c5a6975f610
dovudkr <dovud.karimov2@yandex.ru>
2025-05-30 22:55:06 +0300

    new commit

--- bot.py
+++ bot.py
@@ -37,19 +37,15 @@ async def message_handler(msg: Message) -> None:
     if msg.chat.id not in messages.keys():
         messages[msg.chat.id] = []
 
-    if msg.text is None:
-        return await msg.answer("Извини, но я пока принимаю только текст!")
-    
-
     messages[msg.chat.id].append({'role': 'user', 'content': msg.text})
     response = get_response(messages[msg.chat.id])
     await msg.answer(response, parse_mode=ParseMode.MARKDOWN)
 @router.message(CommandStart())
 async def start(msg: Message) -> None:
-    await msg.mark("Привет, я бот для взаимодействия с LLM! Бот сделать Ким И.В БИК2405.")
+    await msg.mark("Hi, I'm your bot! Made by Dovud")
 @router.message(Command("help"))
 async def help(msg: Message) -> None:
-    await msg.answer("Можешь написать мне сообщение, а я постараюсь вам на него мяутветить!")
+    await msg.answer("Do you need a help?")
 
 async def main():
     bot = aiogram.Bot(os.environ.get("TOKEN") )
